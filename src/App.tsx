0b276960a3dfdae9b685e661910f886e19ad148b
Julian Koripamo <jkoripamo@gmail.com>
2025-05-16 00:05:45 +0100

    remove test line

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#040918] to-[#091540] text-white px-4 py-6">
       <Header />
-      {/* To test that images are working correctly */}
-      <img src="/favicon-32x32.png" alt="" className='h-30 w-30'/>
       <section className="max-w-6xl mx-auto mt-10">
         <ExtensionList />
       </section>
