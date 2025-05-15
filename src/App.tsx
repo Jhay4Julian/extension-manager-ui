470290959187194fc8559ccc255d41a198b5915b
Julian Koripamo <jkoripamo@gmail.com>
2025-05-15 22:39:22 +0100

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-
-import './App.css'
+import './index.css';
+import ExtensionList from './components/ExtensionList';
+import Header from './components/Header';
 
 function App() {
-
   return (
-    <>
-      <p className="bg-blue-100 text-red-700 text-xl">Starter!</p>
-    </>
-  )
+    <div className="min-h-screen bg-gradient-to-b from-[#040918] to-[#091540] text-white px-4 py-6">
+      <Header />
+
+      <section className="max-w-6xl mx-auto mt-10">
+        <ExtensionList />
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
