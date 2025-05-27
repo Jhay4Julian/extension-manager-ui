0285e1693594924b3682e7f1e73791ff531f8903
Julian Koripamo <jkoripamo@gmail.com>
2025-05-27 22:31:40 +0100

    styling update

--- src/components/ExtensionList.tsx
+++ src/components/ExtensionList.tsx
@@ -123,14 +123,14 @@ export default function ExtensionList() {
 
   return (
     <section>
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-white text-2xl font-semibold">Extensions List</h2>
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-6 gap-4">
+        <h2 className="text-black dark:text-white text-2xl font-semibold">Extensions List</h2>
         <div className="flex gap-2">
           {["All", "Active", "Inactive"].map(option => (
             <button
               key={option}
               onClick={() => setFilter(option as any)}
-              className={`px-3 py-1 rounded-full text-sm cursor-pointer ${filter === option ? "bg-red-500 text-black" : "bg-gray-700 text-gray-300 hover:bg-gray-500"
+              className={`px-4 py-2 rounded-full text-sm cursor-pointer ${filter === option ? "bg-red-500 text-black" : "bg-gray-700 text-gray-300 hover:bg-gray-500"
                 }`}
             >
               {option}
